06bc0d8de3776770e8437e5678f6a5c02d3d93c6
ihdzg <143562753+ihdzg@users.noreply.github.com>
2024-03-19 18:36:41 -0600

    Add files via upload

---
+++ EVA2_9_FINAL/src/eva2_9_final/EVA2_9_FINAL.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_final;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA2_9_FINAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Computo producto = new Computo(16, "1.5 TB", "Rizen U5", "Asus", true, "Asus X515", 11500);
+        System.out.println(producto);
+    }
+    
+}
